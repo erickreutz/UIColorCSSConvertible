d6d72800c925e5b5fbb8b3496969518d4d5c9fc0
Eric Kreutzer <eric@airkrft.com>
2014-10-09 16:50:26 -0600

    Initial Commit

---
+++ UIColorCSSConvertible/UIColorCSSConvertible.h
@@ -0,0 +1,19 @@
+//
+//  UIColorCSSConvertible.h
+//  UIColorCSSConvertible
+//
+//  Created by Eric on 10/9/14.
+//  Copyright (c) 2014 erickreutz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIColorCSSConvertible.
+FOUNDATION_EXPORT double UIColorCSSConvertibleVersionNumber;
+
+//! Project version string for UIColorCSSConvertible.
+FOUNDATION_EXPORT const unsigned char UIColorCSSConvertibleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIColorCSSConvertible/PublicHeader.h>
+
+
